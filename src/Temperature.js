8330521255f6a0900af4278724fa64c596cfba78
Sea-Hummingbird <77937964+Sea-Hummingbird@users.noreply.github.com>
2021-05-27 09:36:49 -0400

    fixed API link

--- src/Temperature.js
+++ src/Temperature.js
@@ -25,7 +25,7 @@ export default function Temperature(props) {
 
   function search(){
     const apiKey = "8354e81509710f8d9f63ec83df3a59ad";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
