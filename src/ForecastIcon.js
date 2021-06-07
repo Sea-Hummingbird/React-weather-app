a516a13c9d555ef46a31eb60e0d77061ba89f3e9
Sea-Hummingbird <77937964+Sea-Hummingbird@users.noreply.github.com>
2021-06-07 10:51:52 -0400

    Fixed icons

--- src/ForecastIcon.js
+++ src/ForecastIcon.js
@@ -3,24 +3,24 @@ import "./Forecast.css";
 
 export default function ForecastIcon (props){
     const codeMapping = {
-        "01d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/044/original/clear-sky-f.png?1622678268",
-        "01n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/044/original/clear-sky-f.png?1622678268",
-        "02d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/045/original/few-clouds-f.png?1622678278",
-        "02n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/045/original/few-clouds-f.png?1622678278",
-        "03d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/048/original/scattered-clouds-f.png?1622678311",
-        "03n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/048/original/scattered-clouds-f.png?1622678311",
-        "04d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/043/original/broken-clouds-f.png?1622678255",
-        "04n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/043/original/broken-clouds-f.png?1622678255",
-        "09d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/042/original/shower-rain-f.png?1622678071",
-        "09n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/042/original/shower-rain-f.png?1622678071",
-        "10d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/047/original/rain-f.png?1622678297",
-        "10n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/047/original/rain-f.png?1622678297",
-        "11d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/050/original/thunderstorm-f.png?1622678335",
-        "11n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/050/original/thunderstorm-f.png?1622678335",
-        "13d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/049/original/snow-f.png?1622678325",
-        "13n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/049/original/snow-f.png?1622678325",
-        "50d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/046/original/mist-f.png?1622678288",
-        "50n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/046/original/mist-f.png?1622678288"
+        "01d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/301/original/clear-sky-f.png?1623075568",
+        "01n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/301/original/clear-sky-f.png?1623075568",
+        "02d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/304/original/few-clouds-f.png?1623075605",
+        "02n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/304/original/few-clouds-f.png?1623075605",
+        "03d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/312/original/scattered-clouds-f.png?1623075687",
+        "03n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/312/original/scattered-clouds-f.png?1623075687",
+        "04d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/298/original/broken-clouds-f.png?1623075531",
+        "04n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/298/original/broken-clouds-f.png?1623075531",
+        "09d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/315/original/shower-rain-f.png?1623075729",
+        "09n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/315/original/shower-rain-f.png?1623075729",
+        "10d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/309/original/rain-f.png?1623075655",
+        "10n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/309/original/rain-f.png?1623075655",
+        "11d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/321/original/thunderstorm-f.png?1623075803",
+        "11n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/321/original/thunderstorm-f.png?1623075803",
+        "13d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/318/original/snow-f.png?1623075761",
+        "13n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/318/original/snow-f.png?1623075761",
+        "50d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/306/original/mist-f.png?1623075628",
+        "50n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/306/original/mist-f.png?1623075628"
     }
     return(
         <div>
--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,24 +2,24 @@ import React from "react";
 
 export default function WeatherIcon (props){
     const codeMapping = {
-        "01d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/584/original/clear-sky-d.png?1621993708",
-        "01n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/585/original/clear-sky-n.png?1621993720",
-        "02d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/235/original/few-clouds-d.png?1621556088",
-        "02n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/586/original/few-clouds-n.png?1621993730",
-        "03d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/590/original/scattered-clouds-d.png?1621993774",
-        "03n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/591/original/scattered-clouds-n.png?1621993782",
-        "04d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/582/original/broken-clouds-d.png?1621993686",
-        "04n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/583/original/broken-clouds-n.png?1621993699",
-        "09d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/592/original/shower-rain-d.png?1621993808",
-        "09n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/593/original/shower-rain-n.png?1621993819",
-        "10d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/588/original/rain-d.png?1621993757",
-        "10n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/589/original/rain-n.png?1621993766",
-        "11d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/596/original/thunderstorm-d.png?1621993854",
-        "11n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/597/original/thunderstorm-n.png?1621993864",
-        "13d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/594/original/snow-d.png?1621993831",
-        "13n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/595/original/snow-n.png?1621993844",
-        "50d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/587/original/mist.png?1621993747",
-        "50n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/587/original/mist.png?1621993747"
+        "01d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/300/original/clear-sky-d.png?1623075559",
+        "01n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/302/original/clear-sky-n.png?1623075580",
+        "02d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/303/original/few-clouds-d.png?1623075590",
+        "02n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/305/original/few-clouds-n.png?1623075619",
+        "03d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/311/original/scattered-clouds-d.png?1623075676",
+        "03n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/313/original/scattered-clouds-n.png?1623075695",
+        "04d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/297/original/broken-clouds-d.png?1623075517",
+        "04n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/299/original/broken-clouds-n.png?1623075547",
+        "09d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/314/original/shower-rain-d.png?1623075705",
+        "09n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/316/original/shower-rain-n.png?1623075738",
+        "10d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/308/original/rain-d.png?1623075646",
+        "10n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/310/original/rain-n.png?1623075668",
+        "11d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/320/original/thunderstorm-d.png?1623075784",
+        "11n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/322/original/thunderstorm-n.png?1623075813",
+        "13d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/317/original/snow-d.png?1623075749",
+        "13n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/319/original/snow-n.png?1623075771",
+        "50d": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/307/original/mist.png?1623075637",
+        "50n": "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/010/307/original/mist.png?1623075637"
     }
     return(
         <div>
